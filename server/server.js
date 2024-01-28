d29f56dbb0cb92f08cd1ddbf677076e4489ec796
Parwinder Singh <131915695+PintaAE86@users.noreply.github.com>
2024-01-27 19:46:45 -0500

    create a express server

---
+++ server/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+console.log('sdfasdfasdfasdfasd')
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //add proxy to listen on server on port 3000 or whatever port you setup
 })
