ecc1e80535972a78e984c54e8136293005261ae1
Parwinder Singh <131915695+PintaAE86@users.noreply.github.com>
2024-02-09 19:34:49 -0500

    added data parsing methods added methods for retriving hidden variable

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv'; 
 const app = express();
-const port = 3000;
 
-console.log('sdfasdfasdfasdfasd')
+dotenv.config();//to ready .env files 
+
+const port = process.env.PORT || 3000;
+
+// to read encoded data by standard format used by HTML
+app.use(express.urlencoded({extended : true}));
+//built in method to read parse body 
+app.use(express.json());
+
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
